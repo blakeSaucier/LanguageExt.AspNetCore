5d62744567d55c3ea9271d171f387211124bfb84
James Saucier <james.saucier@lgm.ca>
2019-10-24 17:23:12 -0700

    add option extensions

---
+++ LanguageExt.AspNetCore/Common.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LanguageExt.AspNetCore
+{
+    public static class Common
+    {
+        internal static IActionResult Ok(object result) => new OkObjectResult(result);
+        internal static IActionResult Ok<T>(T t) => new OkObjectResult(t);
+        internal static IActionResult NotFound() => new NotFoundResult();
+        internal static JsonResult OkJson<T>(T t) => new JsonResult(t);
+    }
+}
---
+++ LanguageExt.AspNetCore/OptionExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using static LanguageExt.AspNetCore.Common;
+
+namespace LanguageExt.AspNetCore
+{
+    public static class OptionExtensions
+    {
+        public static IActionResult ToActionResult<T>(this Option<T> option) =>
+            option.Match(Ok, NotFound);
+
+        public static Task<IActionResult> ToActionResult<T>(this Task<Option<T>> option) =>
+            option.Map(ToActionResult);
+
+        public static Option<JsonResult> ToJsonResult<T>(this Option<T> option) =>
+            option.Map(OkJson);
+    }
+}
