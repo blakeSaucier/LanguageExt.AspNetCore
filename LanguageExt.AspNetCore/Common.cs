cfc8ce986a1f6798f3e928e88afa61da7b5c4a98
James Saucier <james.saucier@lgm.ca>
2020-04-05 22:02:12 -0700

    add validation type extensions

--- LanguageExt.AspNetCore/Common.cs
+++ LanguageExt.AspNetCore/Common.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
 
 namespace LanguageExt.AspNetCore
 {
@@ -7,6 +8,12 @@ namespace LanguageExt.AspNetCore
         internal static IActionResult Ok(object result) => new OkObjectResult(result);
         internal static IActionResult Ok<T>(T t) => new OkObjectResult(t);
         internal static IActionResult NotFound() => new NotFoundResult();
+        internal static IActionResult BadRequest<ERROR>(ERROR e) => new BadRequestObjectResult(e);
+        internal static IActionResult BadRequest() => new BadRequestResult();
         internal static JsonResult OkJson<T>(T t) => new JsonResult(t);
+        internal static JsonResult BadRequestJson<E>(E e) => JsonBadRequest(e);
+
+        private static JsonResult JsonBadRequest<E>(E e) =>
+            new JsonResult(e) { StatusCode = StatusCodes.Status400BadRequest };
     }
 }
---
+++ LanguageExt.AspNetCore/ValidationExtensions.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using static LanguageExt.AspNetCore.Common;
+
+namespace LanguageExt.AspNetCore
+{
+    public static class ValidationExtensions
+    {
+        public static IActionResult ToActionResult<FAIL, SUCCESS>(this Validation<FAIL, SUCCESS> validation) =>
+            validation.Match(Ok, BadRequest);
+
+        public static Task<IActionResult> ToActionResult<FAIL, SUCCESS>(this Task<Validation<FAIL, SUCCESS>> validation) =>
+            validation.Map(ToActionResult);
+
+        public static Task<IActionResult> ToActionResult<FAIL, SUCCESS>(this Validation<FAIL, Task<SUCCESS>> validation) =>
+            validation.Sequence().ToActionResult();
+
+        public static Task<IActionResult> ToActionResult<FAIL, SUCCESS>(this Validation<Task<FAIL>, SUCCESS> validation) =>
+            validation.MatchAsync(
+                SuccAsync: a => Ok(a).AsTask(),
+                FailAsync: async e => BadRequest(await e.Sequence()));
+
+        public static JsonResult ToJsonResult<FAIL, SUCCESS>(this Validation<FAIL, SUCCESS> validation) =>
+            validation.Match(OkJson, BadRequestJson);
+        
+        public static Task<JsonResult> ToJsonResult<FAIL, SUCCESS>(this Task<Validation<FAIL, SUCCESS>> validation) =>
+            validation.Map(ToJsonResult);
+    }
+}
