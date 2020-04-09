82fb12ef761664885ab718bf8d3b9ffaa021c463
James Saucier <james.saucier@lgm.ca>
2020-04-08 22:49:30 -0700

    add either extensions

--- LanguageExt.AspNetCore/Common.cs
+++ LanguageExt.AspNetCore/Common.cs
@@ -6,17 +6,32 @@ namespace LanguageExt.AspNetCore
     public static class Common
     {
         internal static IActionResult Ok(object result) => new OkObjectResult(result);
+        
         internal static IActionResult Ok<T>(T t) => new OkObjectResult(t);
+        
         internal static IActionResult NotFound() => new NotFoundResult();
+        
         internal static IActionResult BadRequest<ERROR>(ERROR e) => new BadRequestObjectResult(e);
+        
         internal static IActionResult BadRequest() => new BadRequestResult();
+        
+        internal static IActionResult ServerError<E>(E e) => Error(e);
+        
+        internal static IActionResult ServerError() => Error();
+        
         internal static JsonResult OkJson<T>(T t) => OkJsonResult(t);
+        
         internal static JsonResult BadRequestJson<E>(E e) => BadRequestJsonResult(e);
 
         private static JsonResult OkJsonResult<T>(T t) =>
             new JsonResult(t) { StatusCode = StatusCodes.Status200OK };
-
+        
         private static JsonResult BadRequestJsonResult<E>(E e) =>
             new JsonResult(e) { StatusCode = StatusCodes.Status400BadRequest };
+        
+        private static IActionResult Error() => new StatusCodeResult(StatusCodes.Status500InternalServerError);
+        
+        private static IActionResult Error<E>(E e) =>
+            new ObjectResult(e) { StatusCode = StatusCodes.Status500InternalServerError };
     }
 }
---
+++ LanguageExt.AspNetCore/EitherExtensions.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+using static LanguageExt.AspNetCore.Common;
+
+namespace LanguageExt.AspNetCore
+{
+    public static class EitherExtensions
+    {
+        /// <summary>
+        /// Right case is converted to 200 OK.
+        /// Left case is serialized as a 500 server error.
+        /// </summary>
+        /// <typeparam name="L"></typeparam>
+        /// <typeparam name="R"></typeparam>
+        /// <param name="either"></param>
+        /// <returns></returns>
+        public static IActionResult ToActionResult<L, R>(this Either<L, R> either) =>
+            either.Match(Ok, ServerError);
+
+        /// <summary>
+        /// Right case is converted to 200 OK.
+        /// By default, Left case is returned as 500 server error.
+        /// Alternatively, provide a function to handle the Left case
+        /// </summary>
+        /// <typeparam name="L"></typeparam>
+        /// <typeparam name="R"></typeparam>
+        /// <param name="either"></param>
+        /// <returns></returns>
+        public static IActionResult ToActionResult<L, R>(this Either<L, R> either, Func<L, IActionResult> left = null)
+            => either.Match(Ok, left ?? ServerError);
+
+        /// <summary>
+        /// By default, Right case is converted to 200 OK.
+        /// By default, Left case is returned as 500 server error.
+        /// Alternatively, provide a functions to handle the Left and Right cases.
+        /// </summary>
+        /// <typeparam name="L"></typeparam>
+        /// <typeparam name="R"></typeparam>
+        /// <param name="either"></param>
+        /// <returns></returns>
+        public static IActionResult ToActionResult<L, R>(this Either<L, R> either, Func<L, IActionResult> left = null, Func<R, IActionResult> right = null) =>
+            either.Match(right ?? Ok, left ?? ServerError);
+        
+        /// <summary>
+        /// Right case is converted to 200 OK.
+        /// Left case is serialized as a 500 server error.
+        /// </summary>
+        /// <typeparam name="L"></typeparam>
+        /// <typeparam name="R"></typeparam>
+        /// <param name="either"></param>
+        /// <returns></returns>
+        public static Task<IActionResult> ToActionResult<L, R>(this Task<Either<L, R>> either) =>
+            either.Map(ToActionResult);
+
+        /// <summary>
+        /// Right case is converted to 200 OK.
+        /// By default, Left case is returned as 500 server error.
+        /// Alternatively, provide a function to handle the Left case
+        /// </summary>
+        /// <typeparam name="L"></typeparam>
+        /// <typeparam name="R"></typeparam>
+        /// <param name="either"></param>
+        /// <returns></returns>
+        public static Task<IActionResult> ToActionResult<L, R>(this Task<Either<L, R>> either, Func<L, IActionResult> left = null)
+            => either.Map(v => v.ToActionResult(left));
+
+        /// <summary>
+        /// By default, Right case is converted to 200 OK.
+        /// By default, Left case is returned as 500 server error.
+        /// Alternatively, provide a functions to handle the Left and Right cases.
+        /// </summary>
+        /// <typeparam name="L"></typeparam>
+        /// <typeparam name="R"></typeparam>
+        /// <param name="either"></param>
+        /// <returns></returns>
+        public static Task<IActionResult> ToActionResult<L, R>(this Task<Either<L, R>> either, Func<L, IActionResult> left = null, Func<R, IActionResult> right = null) =>
+            either.Map(v => v.ToActionResult(left, right));
+
+        public static Task<IActionResult> ToActionResult<L, R>(this EitherAsync<L, R> either) =>
+            either.Match(Ok, ServerError);
+    }
+}
