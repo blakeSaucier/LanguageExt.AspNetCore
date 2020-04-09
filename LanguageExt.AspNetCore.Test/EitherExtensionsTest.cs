56dbaa72bda77800c667958f1074bc36d7e2b2b8
James Saucier <james.saucier@lgm.ca>
2020-04-09 14:20:03 -0700

    add either extension tests

---
+++ LanguageExt.AspNetCore.Test/EitherExtensionsTest.cs
@@ -0,0 +1,124 @@
+﻿using FluentAssertions;
+using LanguageExt.Common;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NUnit.Framework;
+using System.Threading.Tasks;
+using static LanguageExt.Prelude;
+
+namespace LanguageExt.AspNetCore.Test
+{
+    public class EitherExtensionsTest
+    {
+        [Test]
+        public void EitherRight_ShouldProduceOk()
+        {
+            // Arrange
+            var right = Right<Error, string>("alt");
+
+            // Act
+            var result = right.ToActionResult();
+
+            // Assert
+            result.Should().BeOfType<OkObjectResult>();
+            (result as OkObjectResult).StatusCode.Should().Be(200);
+        }
+
+        [Test]
+        public void EitherLeft_ShouldProduceError()
+        {
+            // Arrange
+            var left = Left<Error, string>(Error.New("Alt"));
+
+            // Act
+            var result = left.ToActionResult();
+
+            // Assert
+            result.Should().BeOfType<ObjectResult>();
+            (result as ObjectResult).StatusCode.Should().Be(500);
+        }
+
+        [Test]
+        public void EitherLeft_MapLeft()
+        {
+            // Arrange
+            var left = Left<Error, string>(Error.New("Alt"));
+
+            // Act
+            var result = left.ToActionResult(e => new StatusCodeResult(500));
+
+            result.Should().BeOfType<StatusCodeResult>();
+            (result as StatusCodeResult).StatusCode.Should().Be(500);
+        }
+
+        [Test]
+        public void EitherLeft_MapBoth()
+        {
+            // Arrange
+            var left = Left<Error, string>(Error.New("Alt"));
+
+            // Act
+            var result = left.ToActionResult(
+                e => new StatusCodeResult(400),
+                res => new StatusCodeResult(StatusCodes.Status201Created));
+
+            result.Should().BeOfType<StatusCodeResult>();
+            (result as StatusCodeResult).StatusCode.Should().Be(400);
+        }
+        [Test]
+        public async Task TaskEitherRight_ShouldProduceOk()
+        {
+            // Arrange
+            var right = Right<Error, string>("alt").AsTask();
+
+            // Act
+            var result = await right.ToActionResult();
+
+            // Assert
+            result.Should().BeOfType<OkObjectResult>();
+            (result as OkObjectResult).StatusCode.Should().Be(200);
+        }
+
+        [Test]
+        public async Task TaskEitherLeft_ShouldProduceError()
+        {
+            // Arrange
+            var left = Left<Error, string>(Error.New("Alt")).AsTask();
+
+            // Act
+            var result = await left.ToActionResult();
+
+            // Assert
+            result.Should().BeOfType<ObjectResult>();
+            (result as ObjectResult).StatusCode.Should().Be(500);
+        }
+
+        [Test]
+        public async Task TaskEitherLeft_MapLeft()
+        {
+            // Arrange
+            var left = Left<Error, string>(Error.New("Alt")).AsTask();
+
+            // Act
+            var result = await left.ToActionResult(e => new StatusCodeResult(500));
+
+            result.Should().BeOfType<StatusCodeResult>();
+            (result as StatusCodeResult).StatusCode.Should().Be(500);
+        }
+
+        [Test]
+        public async Task TaskEitherLeft_MapBoth()
+        {
+            // Arrange
+            var left = Left<Error, string>(Error.New("Alt")).AsTask();
+
+            // Act
+            var result = await left.ToActionResult(
+                e => new StatusCodeResult(400),
+                res => new StatusCodeResult(StatusCodes.Status201Created));
+
+            result.Should().BeOfType<StatusCodeResult>();
+            (result as StatusCodeResult).StatusCode.Should().Be(400);
+        }
+    }
+}
--- LanguageExt.AspNetCore/Common.cs
+++ LanguageExt.AspNetCore/Common.cs
@@ -5,9 +5,7 @@ namespace LanguageExt.AspNetCore
 {
     public static class Common
     {
-        internal static IActionResult Ok(object result) => new OkObjectResult(result);
-        
-        internal static IActionResult Ok<T>(T t) => new OkObjectResult(t);
+        internal static IActionResult Ok<T>(T t) => (t is Unit) ? NoContent() : OkResult(t);
         
         internal static IActionResult NotFound() => new NotFoundResult();
         
@@ -18,10 +16,14 @@ namespace LanguageExt.AspNetCore
         internal static IActionResult ServerError<E>(E e) => Error(e);
         
         internal static IActionResult ServerError() => Error();
+
+        internal static IActionResult NoContent() => new NoContentResult();
         
         internal static JsonResult OkJson<T>(T t) => OkJsonResult(t);
         
         internal static JsonResult BadRequestJson<E>(E e) => BadRequestJsonResult(e);
+
+        private static IActionResult OkResult<T>(T t) => new OkObjectResult(t);
 
         private static JsonResult OkJsonResult<T>(T t) =>
             new JsonResult(t) { StatusCode = StatusCodes.Status200OK };
