9c99b1afd59a6ac55cf67eceff42daa6e51963cb
James Saucier <james.saucier@lgm.ca>
2020-04-08 22:01:33 -0700

    add documentation for option

--- LanguageExt.AspNetCore/Common.cs
+++ LanguageExt.AspNetCore/Common.cs
@@ -10,8 +10,15 @@ namespace LanguageExt.AspNetCore
         internal static IActionResult NotFound() => new NotFoundResult();
         internal static IActionResult BadRequest<ERROR>(ERROR e) => new BadRequestObjectResult(e);
         internal static IActionResult BadRequest() => new BadRequestResult();
-        internal static JsonResult OkJson<T>(T t) => new JsonResult(t);
+        internal static JsonResult OkJson<T>(T t) => OkJsonResult(t);
         internal static JsonResult BadRequestJson<E>(E e) => JsonBadRequest(e);
+
+
+        private static JsonResult OkJsonResult<T>(T t) =>
+            new JsonResult(t)
+            {
+                StatusCode = StatusCodes.Status200OK
+            };
 
         private static JsonResult JsonBadRequest<E>(E e) =>
             new JsonResult(e) { StatusCode = StatusCodes.Status400BadRequest };
--- LanguageExt.AspNetCore/OptionExtensions.cs
+++ LanguageExt.AspNetCore/OptionExtensions.cs
@@ -6,18 +6,48 @@ namespace LanguageExt.AspNetCore
 {
     public static class OptionExtensions
     {
+        /// <summary>
+        /// Some case is converted to 200 Ok. None case is 404.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>s
+        /// <param name="option"></param>
+        /// <returns></returns>
         public static IActionResult ToActionResult<T>(this Option<T> option) =>
             option.Match(Ok, NotFound);
 
+        /// <summary>
+        /// Some case is converted to 200 Ok. None case is 404.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="option"></param>
+        /// <returns></returns>
         public static Task<IActionResult> ToActionResult<T>(this Task<Option<T>> option) =>
             option.Map(ToActionResult);
 
-        public static Option<JsonResult> ToJsonResult<T>(this Option<T> option) =>
-            option.Map(OkJson);
-
-        public static Task<Option<JsonResult>> ToJsonResult<T>(this Task<Option<T>> option) =>
+        /// <summary>
+        /// Some case is converted to Ok JsonResult. None case is 404.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="option"></param>
+        /// <returns></returns>
+        public static IActionResult ToJsonResult<T>(this Option<T> option) =>
+            option.Match(OkJson, NotFound);
+        
+        /// <summary>
+        /// Some case is converted to Ok JsonResult. None case is 404.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="option"></param>
+        /// <returns></returns>
+        public static Task<IActionResult> ToJsonResult<T>(this Task<Option<T>> option) =>
             option.Map(ToJsonResult);
 
+        /// <summary>
+        /// Some case is converted to Ok JsonResult. None case is 404.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="option"></param>
+        /// <returns></returns>
         public static Task<IActionResult> ToActionResult<T>(this OptionAsync<T> option) =>
             option.Match(Ok, NotFound);
     }
