2aa26307c7d6aca62288756a98a544c482b1e716
James Saucier <james.saucier@lgm.ca>
2020-04-12 15:35:12 -0700

    add try extensions

---
+++ LanguageExt.AspNetCore.Test/TryExtensionsTest.cs
@@ -0,0 +1,119 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using NUnit.Framework;
+using System.Threading.Tasks;
+using static LanguageExt.Prelude;
+
+namespace LanguageExt.AspNetCore.Test
+{
+    public class TryExtensionsTest
+    {
+        [Test]
+        public void TrySuccess_ToActionResult_ShouldBeOk()
+        {
+            // Arrange
+            var sut = Try(() => AverageAge(Writers));
+
+            // Act
+            var result = sut.ToActionResult();
+
+            // Assert
+            result.Should().BeOfType<OkObjectResult>();
+            (result as OkObjectResult).StatusCode.Should().Be(200);
+        }
+
+        [Test]
+        public void TryFail_ToActionResult_ShouldBeServerError()
+        {
+            // Arrange
+            var sut = Try(() => FailingCalculation(Writers));
+
+            // Act
+            var result = sut.ToActionResult();
+
+            // Assert
+            result.Should().BeOfType<ObjectResult>();
+            (result as ObjectResult).StatusCode.Should().Be(500);
+        }
+
+        [Test]
+        public async Task TaskTrySuccess_ToActionResult_ShouldBeOk()
+        {
+            // Arrange
+            var sut = Try(() => AverageAge(Writers)).AsTask();
+
+            // Act
+            var result = await sut.ToActionResult();
+
+            // Assert
+            result.Should().BeOfType<OkObjectResult>();
+            (result as OkObjectResult).StatusCode.Should().Be(200);
+        }
+
+        [Test]
+        public async Task TaskTryFail_ToActionResult_ShouldBeServerError()
+        {
+            // Arrange
+            var sut = Try(() => FailingCalculation(Writers)).AsTask();
+
+            // Act
+            var result = await sut.ToActionResult();
+
+            // Assert
+            result.Should().BeOfType<ObjectResult>();
+            (result as ObjectResult).StatusCode.Should().Be(500);
+        }
+
+        [Test]
+        public async Task TryAsyncSuccess_ToActionResult_ShouldBeOk()
+        {
+            // Arrange
+            var sut = TryAsync(() => AverageAge(Writers).AsTask());
+
+            // Act
+            var result = await sut.ToActionResult();
+
+            // Assert
+            result.Should().BeOfType<OkObjectResult>();
+            (result as OkObjectResult).StatusCode.Should().Be(200);
+        }
+
+        [Test]
+        public async Task TryAsyncFail_ToActionResult_ShouldBeServerError()
+        {
+            // Arrange
+            var sut = TryAsync(() => FailingCalculation(Writers).AsTask());
+
+            // Act
+            var result = await sut.ToActionResult();
+
+            // Assert
+            result.Should().BeOfType<ObjectResult>();
+            (result as ObjectResult).StatusCode.Should().Be(500);
+        }
+
+        private class Person
+        {
+            public Person(string first, string last, int age)
+            {
+                First = first;
+                Last = last;
+                Age = age;
+            }
+
+            public string First { get; }
+            public string Last { get; }
+            public int Age { get; }
+        }
+
+        static Seq<Person> Writers = Seq(
+            new Person("Kurt", "Vonnegut", 30),
+            new Person("T.S.", "Eliot", 40));
+
+        static double AverageAge(Seq<Person> people) =>
+            people.Map(p => p.Age).Average();
+
+        static double FailingCalculation(Seq<Person> people) => 
+            people.Map(p => p.Age).Sum() / 0;
+    }
+}
---
+++ LanguageExt.AspNetCore/TryExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+using static LanguageExt.AspNetCore.Common;
+
+namespace LanguageExt.AspNetCore
+{
+    public static class TryExtensions
+    {
+        public static IActionResult ToActionResult<T>(this Try<T> @try) =>
+            @try.Match(Ok, ServerError);
+        
+        public static Task<IActionResult> ToActionResult<T>(this Task<Try<T>> @try) =>
+            @try.Match(Ok, ServerError);
+
+        public static Task<IActionResult> ToActionResult<T>(this TryAsync<T> @try) =>
+            @try.Match(Ok, ServerError);
+
+        public static Task<IActionResult> ToActionResult<T>(this Try<Task<T>> @try) =>
+            @try.ToAsync().ToActionResult();
+    }
+}
